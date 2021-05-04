bd1002d968cc2f3a0c46d8005f7b82748bc4a0e7
Bardia Eghbali <bardia.eghbali@gmail.com>
2021-05-04 20:20:04 +0430

    fix EXE_Stage Val2

--- LAB1/Val2_Generator.v
+++ LAB1/Val2_Generator.v
@@ -2,13 +2,24 @@ module Val2_Generator (
     input imm,
     input [11:0] Shift_operand,
     input [31:0] Rm_Val,
-    input [3:0] EXE,
-    output [31:0] Val2
+    input MEM_EN,
+    output reg [31:0] Val2
 );
     wire [39:0] rotation_temp = { Shift_operand[7:0], 24'b0, Shift_operand[7:0]};
     wire [39:0] rotated = (rotation_temp >> Shift_operand[11:8]);
-    assign Val2 = (imm == 1) ? rotated[31:0] :
-                                32'b0;
+
+    always @(*) begin
+        if (imm) Val2 = rotated[31:0];
+        else 
+            case (Shift_operand[6:5])
+                2'b00: Val2 = Rm_Val << Shift_operand[11:7];
+                2'b01: Val2 = Rm_Val >> Shift_operand[11:7];
+                2'b10: Val2 = Rm_Val >>> Shift_operand[11:7];
+                2'b11: Val2 = {Rm_Val[Shift_operand[11:7]-1:0], Rm_Val[31:Shift_operand[11:7]]};;
+                default: Val2 = 32'bX;
+            endcase
+    end
+    
 
 endmodule
 
